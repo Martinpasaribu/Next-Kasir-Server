50632ae344217f7f013cacd7f356d185038be6a3
Anugera Rejeki Abadi <martin12082000@gmail.com>
2026-03-03 13:53:53 +0700

    fix log nest js 15

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,41 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+/* eslint-disable @typescript-eslint/require-await */
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
-    const secret = configService.get<string>('JWT_SECRET');
-    
-    // Tambahkan check ini untuk debugging di log Vercel
-    if (!secret) {
-      console.error('❌ FATAL: JWT_SECRET is not defined in environment variables!');
-    }
-
+  constructor() {
     super({
-      // Jika pakai cookie:
-      jwtFromRequest: (req: Request) => {
-        return req?.cookies?.access_token || ExtractJwt.fromAuthHeaderAsBearerToken()(req);
-      },
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: secret,
+      secretOrKey: process.env.JWT_SECRET || 'MARI_BELAJAR_SUPER_SECRET_2026',
     });
   }
 
-  validate(payload: any) {
-    if (!payload) {
-      throw new UnauthorizedException();
-    }
-    return {
-      userId: payload.sub,
-      role: payload.role,
-      tenantId: payload.tenantId,
-      type: payload.type 
+  async validate(payload: any) {
+    // Payload ini adalah isi dari token yang didecode
+    // Apa yang direturn di sini akan masuk ke req.user
+    return { 
+      userId: payload.sub, 
+      email: payload.email, 
+      role: payload.role 
     };
   }
 }
